6dfd8b2719a748be14c26f9a889672a1c769f3cb
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-08-17 14:13:21 -0300

    create multiple files for the tabs dolfer

--- app/(root)/(tabs)/chats.tsx
+++ app/(root)/(tabs)/chats.tsx
@@ -1,10 +1,10 @@
 import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-export const Home = () => {
+export const Chats = () => {
     return (
         <SafeAreaView>
-            <Text>Home</Text>
+            <Text>Chats</Text>
         </SafeAreaView>
     )
 }
