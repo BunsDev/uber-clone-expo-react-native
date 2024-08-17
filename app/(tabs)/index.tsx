8079d3af38e126644a055a1886268fa704e5d2b8
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-08-17 13:40:50 -0300

    configure the package.json to make the app type=module

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { StatusBar } from 'expo-status-bar';
 export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-white">Open up App.js to start working on your app!</Text>
+      <Text className="text-emerald-900 font-bold">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
