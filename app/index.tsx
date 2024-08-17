418553f5977e16c1c90b8acf1ec2969fb7c6ccfe
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-08-17 14:16:51 -0300

    create index.tsx in the app folder as an entry point for the application

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function App() {
+    return (
+        <SafeAreaView>
+            <View className="h-full flex justify-center items-center w-full">
+            <Text className="text-center text-blue-500 font-bold text-3xl">Welcome to Ryde</Text>
+            </View>
+        </SafeAreaView>
+        
+    )
+}
