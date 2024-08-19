649326430539bab1e66c7f6714915477854a932f
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-08-19 10:35:51 -0300

    add backend functionality for signup with clerk

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -2,21 +2,81 @@ import { CustomButton } from "@/components/custom-button";
 import { InputField } from "@/components/input-field";
 import { OAuth } from "@/components/oauth";
 import { icons, images } from "@/constants";
-import { Link } from "expo-router";
+import { useSignUp } from "@clerk/clerk-expo";
+import { Link, useRouter } from "expo-router";
 import { useState } from "react";
-import { View, Text, ScrollView, Image } from "react-native";
+import { View, Text, ScrollView, Image, Alert } from "react-native";
 
 
 export default function SignUp() {
-    const [form, setForm]= useState({
+    const { isLoaded, signUp, setActive } = useSignUp();
+    const [showSuccessModal, setShowSuccessModal] = useState(false);
+
+    const [form, setForm] = useState({
         name: "",
         email: "",
-        password: ""
+        password: "",
+    });
+    const [verification, setVerification] = useState({
+        state: "default",
+        error: "",
+        code: "",
     });
 
     const onSignUpPress = async () => {
-
-    }
+        if (!isLoaded) return;
+        try {
+            await signUp.create({
+                emailAddress: form.email,
+                password: form.password,
+            });
+            await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
+            setVerification({
+                ...verification,
+                state: "pending",
+            });
+        } catch (err: any) {
+            // See https://clerk.com/docs/custom-flows/error-handling
+            // for more info on error handling
+            console.log(JSON.stringify(err, null, 2));
+            Alert.alert("Error", err.errors[0].longMessage);
+        }
+    };
+    const onPressVerify = async () => {
+        if (!isLoaded) return;
+        try {
+            const completeSignUp = await signUp.attemptEmailAddressVerification({
+                code: verification.code,
+            });
+            if (completeSignUp.status === "complete") {
+                // await fetchAPI("/(api)/user", {
+                //     method: "POST",
+                //     body: JSON.stringify({
+                //         name: form.name,
+                //         email: form.email,
+                //         clerkId: completeSignUp.createdUserId,
+                //     }),
+                // });
+                await setActive({ session: completeSignUp.createdSessionId });
+                setVerification({
+                    ...verification,
+                    state: "success",
+                });
+            } else {
+                setVerification({
+                    ...verification,
+                    error: "Verification failed. Please try again.",
+                    state: "failed",
+                });
+            }
+        } catch (err: any) {
+            setVerification({
+                ...verification,
+                error: err.errors[0].longMessage,
+                state: "failed",
+            });
+        }
+    };
 
     return (
         <ScrollView className="flex-1 bg-white">
@@ -30,27 +90,27 @@ export default function SignUp() {
                     </Text>
                 </View>
                 <View className="p-5 ">
-                    <InputField 
+                    <InputField
                         label="Name"
                         icon={icons.person}
                         value={form.name}
                         placeholder="Enter your name"
-                        onChangeText={(text) => setForm({...form, name: text})}
+                        onChangeText={(text) => setForm({ ...form, name: text })}
                     />
-                    <InputField 
+                    <InputField
                         label="Email"
                         icon={icons.email}
                         value={form.email}
                         placeholder="Enter your email"
-                        onChangeText={(text) => setForm({...form, email: text})}
+                        onChangeText={(text) => setForm({ ...form, email: text })}
                     />
-                    <InputField 
+                    <InputField
                         label="Password"
                         icon={icons.lock}
                         value={form.password}
                         secureTextEntry={true}
                         placeholder="**********"
-                        onChangeText={(text) => setForm({...form, password: text})}
+                        onChangeText={(text) => setForm({ ...form, password: text })}
                     />
 
                     <CustomButton
@@ -61,7 +121,7 @@ export default function SignUp() {
 
                     <OAuth />
 
-                    <Link 
+                    <Link
                         href="/sign-in"
                         className="text-lg text-center text-neutral-500 mt-10"
                     >
