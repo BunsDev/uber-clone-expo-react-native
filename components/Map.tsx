0a9efec100ed3d23a91c7bd54f2f6eef1e4d252e
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-08-20 16:17:32 -0300

    integrate GooglePlaces and Direction to GoogleTextInput component

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,15 +1,32 @@
-import { calculateRegion } from "@/lib/map";
-import { useLocationStore } from "@/store";
-import { View, Image } from "react-native"
-import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
+import { drivers, icons } from "@/constants";
+import { calculateRegion, generateMarkersFromData } from "@/lib/map";
+import { useDriverStore, useLocationStore } from "@/store";
+import { MarkerData } from "@/types/type";
+import { useEffect, useState } from "react";
+import MapView, { Marker, PROVIDER_DEFAULT } from "react-native-maps";
 
 export const Map = () => {
-    const { 
+    const {
         userLatitude,
         userLongitude,
         destinationLatitude,
         destinationLongitude
     } = useLocationStore();
+    const { selectedDriver, setDrivers } = useDriverStore();
+    const [markers, setMarkers] = useState<MarkerData[]>([]);
+
+    useEffect(() => {
+        if (Array.isArray(drivers)) {
+            if (!userLatitude || !userLongitude) return;
+            const newMarkers = generateMarkersFromData({
+                data: drivers,
+                userLatitude,
+                userLongitude
+            });
+
+            setMarkers(newMarkers);
+        }
+    }, [drivers])
 
     const region = calculateRegion({
         userLatitude,
@@ -28,7 +45,21 @@ export const Map = () => {
             showsPointsOfInterest={false}
             className="w-full h-full rounded-md"
         >
+            {markers.map((marker) => (
+                <Marker
+                    key={marker.id}
+                    coordinate={{
+                        latitude: marker.latitude,
+                        longitude: marker.longitude
+                    }}
+                    title={marker.title}
+                    image={
+                        selectedDriver === marker.id ? icons.selectedMarker : icons.marker
+                    }
+                >
 
+                </Marker>
+            ))}
         </MapView>
     )
 }
--- components/google-text-input.tsx
+++ components/google-text-input.tsx
@@ -1,5 +1,10 @@
+
+import { icons } from "@/constants";
 import { GoogleInputProps } from "@/types/type"
-import { Text, View } from "react-native"
+import { View, Image } from "react-native"
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+
+const GOOGLE_PLACES_API_KEY = process.env.EXPO_PUBLIC_GOOGLE_API_KEY;
 
 export const GoogleTextInput = ({
     icon,
@@ -7,12 +12,67 @@ export const GoogleTextInput = ({
     containerStyle,
     textInputBackgroundColor,
     handlePress
-} : GoogleInputProps) => {
+}: GoogleInputProps) => {
     return (
         <View className={`flex flex-row items-center justify-center relative z-50 rounded-md mb-5 ${containerStyle}`}>
-            <Text>
-                Search
-            </Text>
+            <GooglePlacesAutocomplete
+                fetchDetails={true}
+                placeholder="Search for a location"
+                debounce={200}
+                styles={{
+                    textInputContainer: {
+                        alignItems: "center",
+                        justifyContent: "center",
+                        borderRadius: 20,
+                        marginHorizontal: 20,
+                        position: "relative",
+                        shadowColor: "#d4d4d4"
+                    },
+                    textInput: {
+                        backgroundColor: textInputBackgroundColor || "white",
+                        fontSize: 16,
+                        fontWeight: 600,
+                        marginTop: 5,
+                        width: "100%",
+                        borderRadius: 200,
+                    },
+                    listView: {
+                        backgroundColor: textInputBackgroundColor || "white",
+                        position: "relative",
+                        top: 0,
+                        width: "100%",
+                        borderRadius: 10,
+                        shadowColor: "#d4d4d4",
+                        zIndex: 99
+                    }
+                }}
+                onPress={(data, details = null) => {
+                    handlePress({
+                        latitude: details?.geometry.location.lat!,
+                        longitude: details?.geometry.location.lng!,
+                        address: data.description
+                    })
+                }}
+                query={{
+                    key: GOOGLE_PLACES_API_KEY,
+                    language: "en",
+                }}
+                renderLeftButton={() => (
+                    <View className="justify-center items-center w-6 h-6">
+                        <Image
+                            source={icon ? icon : icons.search}
+                            alt="Search Icon"
+                            className="w-6 h-6"
+                            resizeMode="contain" 
+                        />
+
+                    </View>
+                )}
+                textInputProps={{
+                    placeholderTextColor: "#d4d4d4",
+                    placeholder: initialLocation ?? "Search for a location"
+                }}
+            />
         </View>
     )
 }
