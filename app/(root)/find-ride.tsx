62427e415ece4894df0c4ea5f713074a09dd98ec
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-08-20 17:03:55 -0300

    design the FindRide page

--- app/(root)/find-ride.tsx
+++ app/(root)/find-ride.tsx
@@ -1,10 +1,51 @@
+import { CustomButton } from "@/components/custom-button";
+import { GoogleTextInput } from "@/components/google-text-input";
 import { RideLayout } from "@/components/ride-layout";
+import { icons } from "@/constants";
+import { useLocationStore } from "@/store";
+import { router } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function FindRide() {
+    const {
+        userAddress,
+        setUserLocation,
+        destinationAddress,
+        setDestinationLocation
+    } = useLocationStore();
     return (
-        <RideLayout title="">
-            <></>
+        <RideLayout title="Ride">
+            <View className="my-3">
+                <Text className="text-lg font-JakartaSemiBold mb-3">
+                    From
+                </Text>
+                <GoogleTextInput
+                    icon={icons.target}
+                    initialLocation={userAddress!}
+                    containerStyle="bg-neutral-100"
+                    textInputBackgroundColor="#F5F5F5"
+                    handlePress={(location) => setUserLocation(location)}
+                />
+            </View>
+
+            <View className="my-3">
+                <Text className="text-lg font-JakartaSemiBold mb-3">
+                    To
+                </Text>
+                <GoogleTextInput
+                    icon={icons.map}
+                    initialLocation={destinationAddress!}
+                    containerStyle="bg-neutral-100"
+                    textInputBackgroundColor="transparent"
+                    handlePress={(location) => setDestinationLocation(location)}
+                />
+            </View>
+
+            <CustomButton
+                title="Find Rides"
+                className="mt-5"
+                onPress={() => router.push("/(root)/find-ride")}
+            />
         </RideLayout>
     )
 }
